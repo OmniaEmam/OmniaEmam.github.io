b20d919048351a6039ea8242546ff01b4ff271bb
OmniaEmam <onieaemam@gmail.com>
2022-02-08 19:45:17 +0200

    Add Files

---
+++ js/gallery.js
@@ -0,0 +1,34 @@
+let galleryImages = document.querySelectorAll(".gallery-img"); 
+let getlatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+
+if(galleryImages) {
+    galleryImages.forEach(function(image , index) {
+          image.onclick = function() {
+               let getElementCss = window.getComputedStyle(image);
+               let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+               let getImgUrlPos = getFullImgUrl.split("/images");
+               let setNewImgUrl = getImgUrlPos[1].replace('")' , '');
+
+               getlatestOpenedImg = index + 1;
+
+               let container = document.body;
+               let newImgWindow = document.createElement("div");
+               container.appendChild(newImgWindow);
+               newImgWindow.setAttribute("class" , "img-window");
+               newImgWindow.setAttribute("onclick" , "closeImg()");
+
+
+               let newImg = document.createElement("img");
+               newImgWindow.appendChild(newImg);
+               newImg.setAttribute("src" , "images/" + setNewImgUrl);
+
+          }
+     });
+    }
+
+function closeImg()
+{
+    document.querySelector(".img-window").remove();
+}    
